2945ab4c0ef22ab7d4a8f56f370d98860631a46f
Lucas Hohmann <lfhohmann@gmail.com>
2024-05-23 20:41:47 -0300

    Rewrote and patched test to check jitter in square wave

--- test.py
+++ test.py
@@ -1,22 +1,77 @@
+import math
+import time
 import unittest
+from unittest.mock import patch
 
 from mock_nidaqmx import DAQ
 
+TEST_DURATION = 11  # In seconds
+MAX_JITTER = 10  # In ms
+PERIOD = 2_000  # In ms
 
-class TestDaq(unittest.TestCase):
+
+class TestSignalJitter(unittest.TestCase):
     """
-    Sample test for the daq class
+    Test that the signal jitter is within acceptable range.
     """
 
-    def test_read(self) -> None:
+    @staticmethod
+    def side_effect_read_digital(*args: list, **kwargs: dict) -> int:
+        """
+        Patch the 'read_digital()' function of the DAQ class to generate a
+        square wave of 0.5Hz and 50% duty cycle.
+
+        Returns:
+            int:
+
+            A 1 or 0 value for HIGH and LOW respectively.
+        """
+
+        return math.floor(time.time() % 2)
+
+    @patch.object(DAQ, "read_digital", side_effect=side_effect_read_digital)
+    def test_read(self, *args: list, **kwargs: dict) -> None:
+
         # Init the device
         device = DAQ()
 
-        # Perform read
-        state = device.read_digital("Dev1/0")
+        # Init the variables to track the sampling process
+        start = time.time()
+        prev_state = None
+        prev_time = start
+        delays = []
 
-        # Check if the result is valid
-        self.assertIn(state, [0, 1])
+        while True:
+            # Store the current time to be constant for each iteration
+            now = time.time()
+
+            # Perform read
+            state = device.read_digital("Dev1/0")
+
+            # Init the previous state if it's None
+            if prev_state is None:
+                prev_state = state
+
+            # Store delay time in list if state changed
+            if state != prev_state:
+                delays.append(now - prev_time)
+                prev_state = state
+                prev_time = now
+
+            # Break the loop if the test duration is reached
+            if (now - start) > TEST_DURATION:
+                break
+
+        # Iterate over each delay and check if it's less then 'MAX_JITTER'.
+        # The first delay is discarded as it did not start synchonized with
+        # the square wave.
+        for delay in delays[1:]:
+
+            # Calculate the jitter
+            jitter = (delay * 1000) - (PERIOD / 2)
+
+            # Assert that the jitter is less then 'MAX_JITTER'
+            self.assertLessEqual(abs(jitter), MAX_JITTER)
 
 
 if __name__ == "__main__":
