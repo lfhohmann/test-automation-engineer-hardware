d0342e62e479e06de9c85fe9350f1400b393b0f5
Lucas Hohmann <lfhohmann@gmail.com>
2024-05-24 11:32:58 -0300

    Mimic the original 'nidaqmx' module

--- mock_nidaqmx/daq.py
+++ mock_nidaqmx/daq.py
@@ -1,54 +1,82 @@
 import random
+from typing import Self
+
+from .utils import unflatten_channel_string
+
+
+class Channel:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    @property
+    def value(self) -> None:
+        raise NotImplementedError()
+
+    def __repr__(self) -> str:
+        return f"Channel({self.name})"
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+
+class DIChannel(Channel):
+    def __init__(self, name) -> None:
+        super().__init__(name)
+
+    @property
+    def value(self) -> int:
+        return random.choice([0, 1])
+
+
+class AIChannel(Channel):
+    def __init__(self, name) -> None:
+        super().__init__(name)
+
+    @property
+    def value(self) -> float:
+        return ((random.randint(0, 2**14) / 2**14) * 10) - 5
+
+
+class AIChannelCollection:
+    def __init__(self, task) -> None:
+        self.task = task
+
+    def add_ai_voltage_chan(self, channels) -> None:
+        channels = unflatten_channel_string(channels)
+
+        for channel in channels:
+            self.task.channels.append(AIChannel(channel))
+
+
+class DIChannelCollection:
+    def __init__(self, task) -> None:
+        self.task = task
+
+    def add_di_chan(self, channels) -> None:
+        channels = unflatten_channel_string(channels)
+
+        for channel in channels:
+            self.task.channels.append(DIChannel(channel))
 
 
 class DAQ:
-    """
-    This class emulates a DAQ device virtually, returning random values for I/O
-    operations.
-    """
+    class Task:
+        def __init__(self) -> None:
+            self.channels: list[Channel] = []
+            self.ai_channels = AIChannelCollection(self)
+            self.di_channels = DIChannelCollection(self)
 
-    # Class constants
-    adc_resolution = 14
-    adc_range = 10
+        def __enter__(self) -> Self:
+            return self
+
+        def __exit__(self, *args, **kwargs) -> None:
+            self.channels = []
+
+        def read(self, number_of_samples_per_channel=1) -> list[list[float | bool]]:
+            return [[channel.value for _ in range(number_of_samples_per_channel)] for channel in self.channels]
 
     def __init__(self, name: str = "Dev1") -> None:
         self.name = name
-
-    def read_analog(self, channel: str) -> float:
-        """
-        Emulate a random value for a 14bit ADC reading.
-
-        Args:
-            channel: The name of the channel to be read (It is here only to
-            simulate the behavior of the original Python library).
-        Returns:
-            float:
-
-            The 14bit value scaled to a range between -5.0 and +5.0 volts.
-
-        """
-
-        value = random.randint(0, 2**self.adc_resolution)
-        value /= 2**self.adc_resolution
-        value *= self.adc_range
-        value -= self.adc_range / 2
-
-        return value
-
-    def read_digital(self, channel: str) -> int:
-        """
-        Emulate a random value for a digital reading.
-
-        Args:
-            channel: The name of the channel to be read (It is here only to
-            simulate the behavior of the original Python library).
-        Returns:
-            int:
-
-            An integer that can be 0 (low) or 1 (high)
-
-        """
-        return random.choice([0, 1])
 
     def __repr__(self) -> str:
         return f"DAQ {self.name}"
